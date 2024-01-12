dd6191a652711720a616dc904aa9290da9703170
Lee Chaeyoung <0929alice@naver.com>
2024-01-13 02:37:33 +0900

    [feat] 메인화면 ui 세팅

--- frontend/app/src/main/java/com/example/auticlever/presenter/main/MainFragment.kt
+++ frontend/app/src/main/java/com/example/auticlever/presenter/main/MainFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.auticlever.R
+
 
 class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
