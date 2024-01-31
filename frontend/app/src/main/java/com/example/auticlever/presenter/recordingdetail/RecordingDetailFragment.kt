0888226cdaeca93424898642c2308e4f0cce1803
leastzero <csong7177@naver.com>
2024-01-31 17:59:18 +0900

    [feat] Memo 2개 실시간 반영 기능 추가

--- frontend/app/src/main/java/com/example/auticlever/presenter/recordingdetail/RecordingDetailFragment.kt
+++ frontend/app/src/main/java/com/example/auticlever/presenter/recordingdetail/RecordingDetailFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -43,27 +45,14 @@ class RecordingDetailFragment : Fragment() {
         }
 
         binding.checkPinning.setOnClickListener{
-            if (binding.checkPinning.isChecked) {
-                binding.checkPinning.setText(R.string.unpinning)
-                binding.checkPinning.setTextColor(ContextCompat.getColor(requireContext(), R.color.primary))
-                binding.etMemo.visibility = View.GONE
-                binding.scrollViewMemo.visibility = View.VISIBLE
-            }
-            else {
-                binding.checkPinning.setText(R.string.pinning)
-                binding.checkPinning.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray4))
-                binding.etMemo.visibility  = View.VISIBLE
-                binding.scrollViewMemo.visibility = View.GONE
-            }
+            CheckPinning()
         }
 
         binding.btnEdit.setOnClickListener{
-            binding.etTitleKeyword.requestFocus()
-
-            val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(binding.etTitleKeyword, InputMethodManager.SHOW_IMPLICIT)
+            KeyboardUp()
         }
 
+        MemoSame()
 
 
         return binding.root
@@ -97,6 +86,54 @@ class RecordingDetailFragment : Fragment() {
         SaveDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         SaveDialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         SaveDialog.show()
+    }
+
+    private fun CheckPinning() {
+        if (binding.checkPinning.isChecked) {
+            binding.checkPinning.setText(R.string.unpinning)
+            binding.checkPinning.setTextColor(ContextCompat.getColor(requireContext(), R.color.primary))
+            binding.etMemo.visibility = View.GONE
+            binding.scrollViewMemo.visibility = View.VISIBLE
+        }
+        else {
+            binding.checkPinning.setText(R.string.pinning)
+            binding.checkPinning.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray4))
+            binding.etMemo.visibility  = View.VISIBLE
+            binding.scrollViewMemo.visibility = View.GONE
+        }
+    }
+
+    private fun KeyboardUp() {
+        binding.etTitleKeyword.requestFocus()
+
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.etTitleKeyword, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun MemoSame() {
+        binding.etMemo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.etBottomMemo.text.toString() != s.toString()) {
+                    binding.etBottomMemo.setText(s)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.etBottomMemo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.etMemo.text.toString() != s.toString()) {
+                    binding.etMemo.setText(s)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
 }
